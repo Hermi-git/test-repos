0be5b140c5281454b9ff0f997bcf3e68b4d205c9
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-08-15 20:29:10 +0300

    Time: 173 ms (99.25%) | Memory: 22.2 MB (16.57%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -0,0 +1,22 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.pings =[]
+        self.p = 0
+
+    def ping(self, t: int) -> int:
+        self.pings.append(t)
+        while self.pings[self.p] < t - 3000:
+            self.p += 1
+        return len(self.pings) - self.p
+
+        
+        
+
+
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
