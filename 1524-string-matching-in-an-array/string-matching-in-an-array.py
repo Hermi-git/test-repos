1cc8aa1cd21387d11e3df3fbe6debe39162d3f8e
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-08-16 09:39:05 +0300

    Time: 88 ms (5.44%) | Memory: 16.9 MB (6.56%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,11 +1,12 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
+        words.sort(key=len)
         matches = []
         for i in range(len(words)):
-            for j in range(len(words)):
-                if words[i] != words[j]:
-                    if re.search(re.escape(words[i]) , words[j]) and words[i] not in matches:
-                        matches.append(words[i])
+            for j in range(i+1,len(words)):
+                if re.search(re.escape(words[i]) , words[j]) and words[i] not in matches:
+                    matches.append(words[i])
+                    break
         return matches
 
         
