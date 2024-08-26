7665be858342325c0f10a4f390ce0cbd4f015e25
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-08-26 22:13:35 +0300

    Time: 38 ms (55.44%) | Memory: 16.6 MB (11.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,12 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
+    # def __init__(self):
+    #     self.memo = {}
+    @lru_cache(None)
     def fib(self, n: int) -> int:
+        # if n <= 1: return n
+        # if n in self.memo:
+        #     return self.memo[n]
+        # self.memo[n] = self.fib(n-2) + self.fib(n-1)
+        # return self.memo[n]
         if n <= 1: return n
-        if n in self.memo:
-            return self.memo[n]
-        self.memo[n] = self.fib(n-2) + self.fib(n-1)
-        return self.memo[n]
+        return self.fib(n-2) + self.fib(n-1)
     
         
          
