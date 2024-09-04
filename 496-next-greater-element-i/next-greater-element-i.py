327026e28a90b5c064550bf99af3330d7dcb0463
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-09-04 15:33:11 +0300

    Time: 53 ms (39.39%) | Memory: 16.9 MB (26.28%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,16 +1,77 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1_dict = {num:index for index, num in enumerate(nums1)}
-        result = [-1] * len(nums1)
+        nums1_dict = {num:i for i, num in enumerate(nums1)}
         stack = []
+        result = [-1] * len(nums1)
         for i in range(len(nums2)):
-            while stack and nums2[i] > stack[-1]:
-                val = stack.pop()
-                idx = nums1_dict[val]
+            while stack and stack[-1] < nums2[i]:
+                value = stack.pop()
+                idx = nums1_dict[value]
                 result[idx] = nums2[i]
-            if nums2[i] in  nums1_dict:
+            if nums2[i] in nums1_dict:
                 stack.append(nums2[i])
         return result
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        # nums1_dict = {}
+        # for index, num in enumerate(nums1):
+        #     nums1_dict[num] = index
+        # result =[-1] * len(nums1)
+        # stack = []
+        # for i in range(len(nums2)):
+        #     while stack and stack[-1] < nums2[i]:
+        #         val = stack.pop()
+        #         idx = nums1_dict[val]
+        #         result[idx] = nums2[i]
+        #     if nums2[i] in nums1_dict:
+        #         stack.append(nums2[i])
+        # return result
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         
 
