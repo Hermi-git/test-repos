6b5322d54998638d52fc8cd3b4546c8edf5f165b
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2024-09-06 11:38:49 +0300

    Time: 871 ms (79.62%) | Memory: 30.8 MB (71.83%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,36 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                answer[idx] = i -idx
+            stack.append(i)
+        return answer
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+
+        
+        
